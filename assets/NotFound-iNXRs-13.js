87c955c2e89d0a228356403f194601439ec141c7
FeelsBotMan <sjb0229@gmail.com>
2024-11-11 09:31:55 +0900

    Updates

---
+++ assets/NotFound-iNXRs-13.js
@@ -0,0 +1,1 @@
+import{b as t,j as a,E as r}from"./index-DD9nCpU9.js";var e=function(){var u=t();return a.jsx(r,{title:"404",message:"페이지를 찾을 수 없습니다",onButtonClick:function(){return u("/")}})};export{e as default};
