1476cfaa8ae9c273d34320dd6ad9165806feda9e
FeelsBotMan <sjb0229@gmail.com>
2024-11-12 11:13:49 +0900

    Updates

--- assets/Cart-CU6-rwW6.js
+++ assets/Cart-CU6-rwW6.js
@@ -1,4 +1,4 @@
-import{c as t,r,j as s,L as i}from"./index-CqUpMUIQ.js";import{A as l}from"./arrow-right-KnOWS7Oz.js";/**
+import{c as t,r,j as s,L as i}from"./index-BR1Csf5A.js";import{A as l}from"./arrow-right-BnktX18m.js";/**
  * @license lucide-react v0.456.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/arrow-right-BnktX18m.js
+++ assets/arrow-right-BnktX18m.js
@@ -1,4 +1,4 @@
-import{c as r}from"./index-CqUpMUIQ.js";/**
+import{c as r}from"./index-BR1Csf5A.js";/**
  * @license lucide-react v0.456.0 - ISC
  *
  * This source code is licensed under the ISC license.
